4d46aee0790ab302605623a562655c167cb52cc8
tawon2137 <ziezonn@naver.com>
2018-03-26 21:48:06 +0900

    온오프 클래스 반영

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 (function(w, d) {
 
-
     if(!(w && d)) {
         return false;
     }
@@ -134,7 +133,7 @@
                 this._el.setAttribute('data-y', position.y);
                 t.to(this._el, 0, { x: position.x, y: position.y, onComplete: _ => {
                     const center = { x: (w.innerWidth / 2) - (this._el.offsetWidth / 2), y: (w.innerHeight / 2) - (this._el.offsetHeight / 2)};
-                    t.to(this._el, this.option.duration, { visibility: 'visible', opacity: 1, scale: 1, x: center.x, y: center.y, ease: this.option.ease, zIndex: 400 });
+                    t.to(this._el, this.option.duration, { visibility: 'visible', opacity: 1, scale: 1, x: center.x, y: center.y, ease: this.option.ease, zIndex: 400, onComplete: _ => aClass(this._el, 'open') });
                     t.to(this._dim, this.option.duration, { display: 'inline-block', opacity: 1, ease: this.option.ease });
                 } });
             });
@@ -143,13 +142,14 @@
         close() {
             t.to(this._el, this.option.duration, { opacity: 0, scale: 0.7, x: this._el.getAttribute('data-x'),y: this._el.getAttribute('data-y'), zIndex: -1, ease: this.option.ease });
             t.to(this._dim, this.option.duration, { display: 'none', opacity: 0, ease: this.option.ease, onComplete: _ => {
-                document.body.style.overflow = '';
-                this._el.style.transform = 'translate(-100%,-100%);';
-                this._el.visibility = 'hidden';
-                this._el.removeAttribute('data-x');
-                this._el.removeAttribute('data-y');
-                this._dim.parentElement && this._dim.parentElement.removeChild(this._dim);
-            }
+                    document.body.style.overflow = '';
+                    this._el.style.transform = 'translate(-100%,-100%);';
+                    this._el.visibility = 'hidden';
+                    this._el.removeAttribute('data-x');
+                    this._el.removeAttribute('data-y');
+                    this._dim.parentElement && this._dim.parentElement.removeChild(this._dim);
+                    hClass(this._el, 'open') && aClass(this._el, 'open');
+                }
             });
         }
 
